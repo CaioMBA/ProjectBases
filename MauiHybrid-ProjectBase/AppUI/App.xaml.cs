764e66254ae15fd4922944b969b131955801873d
Caio <caiombaraujo@gmail.com>
2025-07-25 15:58:14 -0300

    fix newest MainPage.xaml

--- MauiHybrid-ProjectBase/AppUI/App.xaml.cs
+++ MauiHybrid-ProjectBase/AppUI/App.xaml.cs
@@ -1,6 +1,4 @@
-﻿using Data.DatabaseRepositories.EntityFrameworkContexts;
-using Domain.Models.ApplicationConfigurationModels;
-using Microsoft.EntityFrameworkCore;
+﻿using Domain.Models.ApplicationConfigurationModels;
 
 namespace AppUI
 {
